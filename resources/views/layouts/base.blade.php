37c1fe90bb433bccd5db40f9eea5b270ef5c7a70
Jasim Molla <jasimibos@gmail.com>
2021-09-22 11:33:24 +0600

    commit

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -21,7 +21,7 @@
     <link rel="stylesheet" type="text/css" href="{{asset('assets/css/style.css')}}">
     <link rel="stylesheet" type="text/css" href="{{asset('assets/css/color-01.css')}}">
     @livewireStyles
-  
+
 </head>
 
 <body class="home-page home-01 ">
@@ -96,8 +96,8 @@
                     <div class="mid-section main-info-area">
 
                         <div class="wrap-logo-top left-section">
-                            <a href="index.html" class="link-to-home"><img src="{{asset('assets/images/logo-top-1.png')}}"
-                                    alt="mercado"></a>
+                            <a href="index.html" class="link-to-home"><img
+                                    src="{{asset('assets/images/logo-top-1.png')}}" alt="mercado"></a>
                         </div>
 
                         <div class="wrap-search center-section">
@@ -186,20 +186,20 @@
                         <div class="container">
                             <ul class="nav primary clone-main-menu" id="mercado_main" data-menuname="Main menu">
                                 <li class="menu-item home-icon">
-                                    <a href="index.html" class="link-term mercado-item-title"><i class="fa fa-home"
+                                    <a href="/" class="link-term mercado-item-title"><i class="fa fa-home"
                                             aria-hidden="true"></i></a>
                                 </li>
                                 <li class="menu-item">
                                     <a href="about-us.html" class="link-term mercado-item-title">About Us</a>
                                 </li>
                                 <li class="menu-item">
-                                    <a href="shop.html" class="link-term mercado-item-title">Shop</a>
+                                    <a href="/shop" class="link-term mercado-item-title">Shop</a>
                                 </li>
                                 <li class="menu-item">
-                                    <a href="cart.html" class="link-term mercado-item-title">Cart</a>
+                                    <a href="/cart" class="link-term mercado-item-title">Cart</a>
                                 </li>
                                 <li class="menu-item">
-                                    <a href="checkout.html" class="link-term mercado-item-title">Checkout</a>
+                                    <a href="/checkout" class="link-term mercado-item-title">Checkout</a>
                                 </li>
                                 <li class="menu-item">
                                     <a href="contact-us.html" class="link-term mercado-item-title">Contact Us</a>
